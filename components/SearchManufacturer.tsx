aa9aa2ca7ad5eea7ab6d0188116f80af1374a9fd
Florin <florinconstantin99@yahoo.com>
2023-08-31 10:55:30 +0300

    initial commit

---
+++ components/SearchManufacturer.tsx
@@ -0,0 +1,69 @@
+"use client";
+
+import { SearchManufacturerProps } from "@/types";
+import { Combobox, Transition } from "@headlessui/react";
+import { manufacturers } from "@/constants";
+import React, { Fragment, useState } from "react";
+import Image from "next/image";
+
+function SearchManufacturer({
+  manufacturer,
+  setManufacturer,
+}: SearchManufacturerProps) {
+  const [query, setQuery] = useState("");
+
+  const filteredManufacturers =
+    query === ""
+      ? manufacturers
+      : manufacturers.filter((item) =>
+          item
+            .toLowerCase()
+            .replace(/\s+/g, "")
+            .includes(query.toLowerCase().replace(/\s+/g, ""))
+        );
+
+  return (
+    <div className="search-manufacturer">
+      <Combobox>
+        <div className="relative w-full">
+          <Combobox.Button className="absolute top-[14px]">
+            <Image
+              src="/car-logo.svg"
+              width={20}
+              height={20}
+              className="ml-4"
+              alt="Car logo"
+            />
+          </Combobox.Button>
+          <Combobox.Input
+            className="search-manufacturer__input"
+            placeholder="Type here"
+            displayValue={(manufacturer: string) => manufacturer}
+            onChange={(e) => setQuery(e.target.value)}
+          ></Combobox.Input>
+
+          <Transition
+            as={Fragment}
+            leave="transition ease in duration-100"
+            leaveFrom="opacity-100"
+            leaveTo="opacity-0"
+            afterLeave={() => setQuery("")}
+          >
+            <Combobox.Options>
+              {filteredManufacturers.length === 0 && query !== "" ? (
+                <Combobox.Option
+                  value={query}
+                  className="search-manufacturer__option"
+                >
+                  Create "{query}"
+                </Combobox.Option>
+              ) : (filteredManufacturers.map((item) =>  )}
+            </Combobox.Options>
+          </Transition>
+        </div>
+      </Combobox>
+    </div>
+  );
+}
+
+export default SearchManufacturer;
